7d709d57ea767f3e0f6465d9782e0d7fc42ba885
Lewis <lewisbirch2@googlemail.com>
2025-03-25 10:36:06 +0000

    Configured Header Setup

--- launchpad/src/Components/App.jsx
+++ launchpad/src/Components/App.jsx
@@ -1,13 +1,12 @@
 import "./App.css";
-import Header from "./Header"
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="Title">
-      <header className="TitleText">
-        <p>Museum Exhibition Project</p>
-      </header>
-    </div>
+    <>
+      <Header />
+    
+    </>
   );
 }
 
--- launchpad/src/Components/Header.jsx
+++ launchpad/src/Components/Header.jsx
@@ -1,3 +1,13 @@
-const Header = (props) => {
-  return <h1> {props.name}</h1>;
+const Header = () => {
+  return (
+    <>
+      <h1> Welcome to the Museum Collective Exhibition</h1>
+      <h2>
+        Feel free to browse selected artworks, or begin to build your own
+        gallery
+      </h2>
+    </>
+  );
 };
+
+export default Header;
