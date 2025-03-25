f1489f8e68792569f5c559a97f93b191c67aafb0
Lewis <lewisbirch2@googlemail.com>
2025-03-25 11:59:57 +0000

    Setting up endpoints

--- launchpad/src/Components/App.jsx
+++ launchpad/src/Components/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import Header from "./Header";
+import About from "./About";
+import Gallery from "./Gallery";
+import MyExhibit from "./MyExhibit";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-    
+      <Routes>
+        <Route path="/" />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/myexhibit" element={<MyExhibit />} />
+      </Routes>
     </>
   );
 }
---
+++ launchpad/src/Components/MyExhibit.jsx
@@ -0,0 +1,3 @@
+export default function MyExhibit(){
+    return <h1>MyExhibit</h1>
+}
