bbc0ab6edd18eed1d1bae03c433fcb4b9bf65412
Lewis <lewisbirch2@googlemail.com>
2025-04-07 14:28:13 +0100

    continue DB setup

--- launchpad/server/connect.js
+++ launchpad/server/connect.js
@@ -1,21 +1,32 @@
 const { MongoClient } = require("mongodb");
 require("dotenv").config({ path: "./config.env" });
 
-async function main() {
-  const secret = process.env.ATLAS_URI;
-  const client = new MongoClient(secret);
+const secret = process.env.ATLAS_URI;
+const client = new MongoClient(secret);
 
-  try {
-    await client.connect();
-    const collections = await client.db("Museums").collections();
-    collections.forEach((collection) =>
-      console.log(collection.s.namespace.collection)
-    );
-  } catch (e) {
-    console.log(e);
-  } finally {
-    await client.close();
-  }
-}
+let database;
 
-main();
+module.exports = {
+  connectToServer: () => {
+    database = client.db("Museums");
+  },
+  getDb: () => {
+    return database;
+  },
+};
+
+//async function main() {
+//   try {
+//     await client.connect();
+//     const collections = await client.db("Museums").collections();
+//     collections.forEach((collection) =>
+//       console.log(collection.s.namespace.collection)
+//     );
+//   } catch (e) {
+//     console.log(e);
+//   } finally {
+//     await client.close();
+//   }
+// }}
+
+// main();
