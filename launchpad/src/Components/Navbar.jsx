c2f9e6eb76d4eced0c00680dd43ea7937041160e
Lewis <lewisbirch2@googlemail.com>
2025-04-15 12:45:25 +0100

    logout button

--- launchpad/src/Components/Navbar.jsx
+++ launchpad/src/Components/Navbar.jsx
@@ -1,7 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { pageData } from "./PageData";
+import { useNavigate } from "react-router-dom";
 
 export function Navbar() {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.removeItem("User");
+    navigate("/");
+  }
+
   return (
     <div className="navbar">
       {pageData.map((page) => {
@@ -11,6 +19,7 @@ export function Navbar() {
           </Link>
         );
       })}
+      <button onClick={handleLogout}>Log Out</button>
     </div>
   );
 }
