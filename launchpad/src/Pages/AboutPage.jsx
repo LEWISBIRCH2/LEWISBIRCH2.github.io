a11510e39fde2aabe3b6530eab76338bd646f5a6
Lewis <lewisbirch2@googlemail.com>
2025-05-01 16:35:23 +0100

    About page styling

--- launchpad/src/Pages/AboutPage.jsx
+++ launchpad/src/Pages/AboutPage.jsx
@@ -1,21 +1,25 @@
 export function AboutPage() {
   return (
-    <div className="about">
-      <h1>About The Platform</h1>
-      <p>
-        {" "}
-        Welcome to this Exhibition Curator platform! Here, you're able to
-        explore a range of artworks and exhibits from various museums
-      </p>
-      <p>
-        This platform also allows you structure your own personal exhibit,
-        filled with artworks catered to your taste
-      </p>
-      <p>
-        {" "}
-        This project was completed as part of the Launchpad program, which
-        allows trainee developers to demonstrate their abilities
-      </p>
-    </div>
+    <>
+      <div className="about-wrapper">
+        <div className="about">
+          <h1>About The Platform</h1>
+          <p>
+            {" "}
+            Welcome to this Exhibition Curator platform! Here, you're able to
+            explore a range of artworks and exhibits from various museums
+          </p>
+          <p>
+            This platform also allows you structure your own personal exhibit,
+            filled with artworks catered to your taste
+          </p>
+          <p>
+            {" "}
+            This project was completed as part of the Launchpad program, which
+            allows trainee developers to demonstrate their abilities
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
