b99d4110b186db3ae6fdfa6bace79008240fcd22
Lewis <lewisbirch2@googlemail.com>
2025-05-05 21:10:52 +0100

    Updates

--- launchpad/src/Components/Login.jsx
+++ launchpad/src/Components/Login.jsx
@@ -19,7 +19,7 @@ export function Login() {
     let response = await verifyUser(user);
     if (response) {
       localStorage.setItem("User", response);
-      navigate("https://lewisbirch2.github.io/Launchpad/home");
+      navigate("https://lewisbirch2.github.io/Launchpad/#/home");
     } else {
       alert("Incorrect details submitted");
     }
