4eb2b1f315bbea76326464c0ad8c1fd8c75d2f66
Lewis Birch <lewisbirch2@googlemail.com>
2025-05-05 17:13:50 +0100

    Delete Launchpad.jsx

--- Launchpad.jsx
+++
@@ -1,8 +0,0 @@
-/* Connection string: 
-
-mongodb+srv://lewisbirch2:ZfdeOAgY2VRyfo9O@exhibitionproject.67uj6.mongodb.net/?retryWrites=true&w=majority&appName=ExhibitionProject
-
-PASSWORD IS VISIBLE */
-
-// test2
-// T3
